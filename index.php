0a185251946cd6cffddee03385c8adfa70bd78da
ThePopato <72664761+ThePopato@users.noreply.github.com>
2020-11-30 16:27:46 +0300

    Php format for logging on and registering users.

--- index.php
+++ index.php
@@ -1,73 +1,87 @@
-<?php  include('create_conferenceroles.php'); ?>
-<?php
-$db = mysqli_connect('localhost', 'root', '', 'odev2');
-if (isset($_GET['edit'])) {
-    $ConfIDRole = $_GET['edit'];
-    $update = true;
-    $record = mysqli_query($db, "SELECT * FROM conferenceroles WHERE ConfIDRole=$ConfIDRole");
-
-    if (count($record) == 1 ) {
-        $n = mysqli_fetch_array($record);
-        $ConfIDRole = $n['ConfIDRole'];
-        $AuthenticationID = $n['AuthenticationID'];
-    }
-}
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>CRUD: CReate, Update, Delete PHP MySQL</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
 </head>
 <body>
-<?php if (isset($_SESSION['message'])): ?>
-    <div class="msg">
-        <?php
-        echo $_SESSION['message'];
-        unset($_SESSION['message']);
-        ?>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left">Employees Details</h2>
+                        <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+                    
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM employees";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>Name</th>";
+                                        echo "<th>Address</th>";
+                                        echo "<th>Salary</th>";
+                                        echo "<th>Action</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['name'] . "</td>";
+                                        echo "<td>" . $row['address'] . "</td>";
+                                        echo "<td>" . $row['salary'] . "</td>";
+                                        echo "<td>";
+                                            echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                            echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                    }
+ 
+                    // Close connection
+                    mysqli_close($link);
+                    ?>
+                </div>
+            </div>        
+        </div>
     </div>
-<?php endif ?>
-<?php $db =mysqli_connect('localhost', 'root', '', 'crud');
-$results = mysqli_query($db, "SELECT * FROM conferenceroles"); ?>
-<table>
-    <thead>
-    <tr>
-        <th>ConfIDRole</th>
-        <th>Address</th>
-        <th colspan="2">Action</th>
-    </tr>
-    </thead>
-
-    <?php while ($row = mysqli_fetch_array($results)) { ?>
-        <tr>
-            <td><?php echo $row['ConfIDRole']; ?></td>
-            <td><?php echo $row['AuthenticationID']; ?></td>
-            <td>
-                <a href="index.php?edit=<?php echo $row['ConfIDRole']; ?>" class="edit_btn" >Edit</a>
-            </td>
-            <td>
-                <a href="server.php?del=<?php echo $row['ConfIDRole']; ?>" class="del_btn">Delete</a>
-            </td>
-        </tr>
-    <?php } ?>
-</table>
-<form method="post" action="create_conferenceroles.php" >
-    <div class="input-group">
-        <label>ConfIDRole</label>
-        <input type="number" name="ConfIDRole" value="<?php echo $ConfIDRole; ?>">
-    </div>
-    <div class="input-group">
-        <label>AuthenticationID</label>
-        <input type="number" name="AuthenticationID" value="<?php echo $AuthenticationID; ?>">
-    </div>
-    <div class="input-group">
-        <?php if ($update == true): ?>
-            <button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
-        <?php else: ?>
-            <button class="btn" type="submit" name="save" >Save</button>
-        <?php endif ?>
-    </div>
-</form>
 </body>
 </html>
