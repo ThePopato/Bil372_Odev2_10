3d7e3a6c5ccd0374b5c2ccd2806b031e7213fee4
ThePopato <72664761+ThePopato@users.noreply.github.com>
2020-12-02 01:28:42 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,115 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
-// Initialize the session
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require('config.php');
 session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
-}
- 
-// Include config file
-require_once "config.php";
- 
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT authenticationID, username, password FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $AuthenticationID, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["authid"] = $authenticationID;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-    // Close connection
-    mysqli_close($link);
-}
+// If form submitted, insert values into the database.
+if (isset($_POST['username'])){
+        // removes backslashes
+ $username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+ $username = mysqli_real_escape_string($link,$username);
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($link,$password);
+ //Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE username='$username' and password='".md5($password)."'";
+ $result = mysqli_query($link,$query) or die(mysql_error());
+ $rows = mysqli_num_rows($result);
+        if($rows==1){
+     $_SESSION['username'] = $username;
+            // Redirect user to index.php
+            $_SESSION['loggedin']=true;
+     header("Location: welcome.php");
+         }else{
+ echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+ }
+    }else{
 ?>
- 
- <!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <title>Kullanici Girisi</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="{{url_for('static', filename='main.css')}}">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-  <div class="container">
-    <form class="form-signin_up" method="post" action="#">
-      <h2 class="form-signin_up-heading">Lütfen giriş yapınız.</h2>
-	  <label></label>
-      <input type="text" class="form-control" name="username" placeholder="Kullanıcı adı" required="" autofocus="" />
-	  <label></label>
-      <input type="password" class="form-control" name="password" placeholder="Şifre" required=""/>   
-	  <label></label>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Giriş Yap</button>
-	  <label></label>
-	  <a href="register.php">Hesabınız yok mu, kayıt olun!</a>
-    </form>
-  </div>
+<div class="form">
+<h1>Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Username" required />
+<input type="password" name="password" placeholder="Password" required />
+<input name="submit" type="submit" value="Login" />
+</form>
+<p>Not registered yet? <a href='register.php'>Register Here</a></p>
+</div>
+<?php } ?>
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,224 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Kayıt Olun!</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
-// Include config file
-require_once "config.php";
+require('config.php');
+// If form submitted, insert values into the database.
+if (isset($_REQUEST['username'])){
+        // removes backslashes
+ $username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+ $username = mysqli_real_escape_string($link,$username); 
  
-// Define variables and initialize with empty values
-$name = $password = $affiliation = $title = $personname = $email1 = $email2 = $phone = $address = 
-$country = $city = $confirm_password = "";
-$name_err = $password_err = $confirm_aff_err = $confirm_title_err = $confirm_personname_err = 
-$confirm_mail1_err = $confirm_mail2_err = $confirm_phone_err = $confirm_fax_err = 
-$confirm_address_err = $confirm_country_err = $confirm_city_err =  $confirm_password_err = "";
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($link,$password);
+
+ $affiliation = stripslashes($_REQUEST['affiliation']);
+ $affiliation = mysqli_real_escape_string($link,$affiliation);
+
+ $title = stripslashes($_REQUEST['title']);
+ $title = mysqli_real_escape_string($link,$title);
+
+ $personname = stripslashes($_REQUEST['personName']);
+ $personname = mysqli_real_escape_string($link,$personname);
+
+ $personsurname = stripslashes($_REQUEST['personSurname']);
+ $personsurname = mysqli_real_escape_string($link,$personsurname);
+
+ $email1 = stripslashes($_REQUEST['email1']);
+ $email1 = mysqli_real_escape_string($link,$email1);
+
+ $email2 = stripslashes($_REQUEST['email2']);
+ $email2 = mysqli_real_escape_string($link,$email2);
  
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+ $phone = stripslashes($_REQUEST['phone']);
+ $phone = mysqli_real_escape_string($link,$phone);
+
+ $fax = stripslashes($_REQUEST['fax']);
+ $fax = mysqli_real_escape_string($link,$fax);
+
+ $address = stripslashes($_REQUEST['address']);
+ $address = mysqli_real_escape_string($link,$address);
+
+ $country = stripslashes($_REQUEST['country']);
+ $country = mysqli_real_escape_string($link,$country);
+
+ $city = stripslashes($_REQUEST['city']);
+ $city = mysqli_real_escape_string($link,$city);
+
+ $date = date("Y-m-d H:i:s");
  
-    // Validate username
-    if(empty(trim($_POST["name"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT authentication_id, username FROM users WHERE username = ?";
+ $query_users = "INSERT into `users` (username, password) VALUES ('$username', '".md5($password)."')";
+ 
+ $query_userlog = "INSERT into `userlog` 
+ (Title, `Name`, LastName, Affiliation primary_email, secondary_email, `password`,
+ phone, fax, `URL`, `Address`, `City`, Country, Record, Creation, `Date`) 
+ VALUES ('$title', '$personname', '$personsurname', '$email1', '$email2', '".md5($password)."',
+ '$phone', '$fax', 'url', '$address', '$city', '$country', 'record', 'creation',  '$date')";
+ 
+ $query_usersinfo = "INSERT into `usersinfo` (Title, `Name`, LastName, Affiliation primary_email, secondary_email, `password`,
+ phone, fax, `URL`, `Address`, `City`, Country, Record, Creation, `Date`) 
+ VALUES ('$title', '$personname', '$personsurname', '$email1', '$email2', '".md5($password)."',
+ '$phone', '$fax', 'url', '$address', '$city', '$country', 'record', 'creation',  '$date')";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_name);
-            
-            // Set parameters
-            $param_name = trim($_POST["name"]);
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["name"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
+        $result1 = mysqli_query($link,$query_users);
+        $result2 = mysqli_query($link,$query_userlog);
+        $result3 = mysqli_query($link,$query_usersinfo);
+        if($result1){
+            echo "<div class='form'>
+<h3>You are registered successfully.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
         }
-    }
-    
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    if(empty(trim($_POST["affiliation"]))){
-        $confirm_aff_err = "Please insert affiliation.";     
-    }
-    else{
-        $affiliation = trim($_POST["affiliation"]);
-    }
-    
-    if(empty(trim($_POST["title"]))){
-        $confirm_title_err = "Please insert title.";     
-    }
-    else{
-        $title = trim($_POST["title"]);
-    }
-    
-    if(empty(trim($_POST["responsName"]))){
-        $confirm_personname_err = "Please insert name and surname.";     
-    }
-    else{
-        $personname = trim($_POST["responsName"]);
-    }
-
-    if(empty(trim($_POST["email1"]))){
-        $confirm_mail1_err = "Please insert a valid email address.";     
-    }
-    else{
-        $email1 = trim($_POST["email1"]);
-    }
-
-    if(empty(trim($_POST["email2"]))){
-        $confirm_mail2_err = "Please insert a valid email address.";     
-    }
-    else{
-        $email2 = trim($_POST["email2"]);
-    }
-
-    if(empty(trim($_POST["phone"]))){
-        $confirm_phone_err = "Please insert a valid phone number.";     
-    }
-    else{
-        $phone = trim($_POST["phone"]);
-    }
-
-    if(empty(trim($_POST["faks"]))){
-        $confirm_fax_err = "Please insert a valid fax number.";     
-    }
-    else{
-        $fax = trim($_POST["faks"]);
-    }
-
-    if(empty(trim($_POST["address"]))){
-        $confirm_address_err = "Please insert a valid address.";     
-    }
-    else{
-        $address = trim($_POST["address"]);
-    }
-
-    if(empty(trim($_POST["ulkeAdi"]))){
-        $confirm_country_err = "Please insert a country.";     
-    }
-    else{
-        $country = trim($_POST["ulkeAdi"]);
-    }
-
-    if(empty(trim($_POST["sehir"]))){
-        $confirm_city_err = "Please insert a city.";     
-    }
-    else{
-        $city = trim($_POST["sehir"]);
-    }
-
-
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)
-    && empty($confirm_aff_err) && empty($confirm_title_err) && empty($confirm_personname_err)
-    && empty($confirm_mail1_err) && empty($confirm_mail2_err) && empty($confirm_phone_err)
-    && empty($confirm_fax_err) && empty($confirm_address_err) && empty($confirm_country_err)
-    && empty($confirm_city_err)){
-        
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password, affiliation, title, personname, email1
-                , email2, phone, fax, address, country, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password, 
-            $param_aff, $param_title, $param_name, $param_mail1
-            , $param_mail2, $param_phone, $param_fax, $param_address, $param_country
-            , $param_city);
-            
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            $param_aff = $affiliation;
-            $param_title = $title;
-            $param_name = $personname;
-            $param_mail1 = $email1;
-            $param_mail2 = $email2;
-            $param_phone = $phone;
-            $param_fax = $fax;
-            $param_address = $address;
-            $param_country = $country;
-            $param_city = $city;
-
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: login.php");
-            } else{
-                echo "Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-    // Close connection
-    mysqli_close($link);
-}
+    }else{
 ?>
- 
- <!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <title>Kayıt Olun!</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="{{url_for('static', filename='main.css')}}">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-  <div class="container">
-    <form class="form-signin_up" method="post" action="#">       
-      <h2 class="form-signin_up-heading">Yeni Hesap Oluştur.</h2>
-	  <br></br>
-	  <label>Kullanıcı Adı: </label>
-      <input type="text" class="form-control" name="name" required="" autofocus="" />
-	  <label>Sifre: </label>
-      <input type="password" class="form-control" name="password" required="" autofocus="" />
-	  <label>Kurum Adı: </label>
-      <input type="text" class="form-control" name="affiliation " required="" autofocus="" />
-	  <label>Unvan: </label>
-      <input type="text" class="form-control" name="title" required="" autofocus="" />
-	  <label>Adı/Soyadı: </label>
-      <input type="text" class="form-control" name="responsName" required=""/>
-	  <label>Email: </label>
-	  <input type="email" class="form-control" name="email1" required="" autofocus="" />
-	  <label>Ikinci Email: </label>
-	  <input type="email" class="form-control" name="email2" required="" autofocus="" />
-	  <label>Telefon: </label>
-	  <input type="tel" class="form-control" name="phone" required="" autofocus="" />
-	  <label>Faks: </label>
-	  <input type="tel" class="form-control" name="faks" required="" autofocus="" />
-	  <label>Adres: </label>
-	  <textarea class="form-control" id="adres" name="address" rows="3"></textarea>
-	  <label>Ulke: </label>
-      <input type="text" class="form-control" name="ulkeAdi" required=""/>
-	  <label>Sehir: </label>
-      <input type="text" class="form-control" name="sehir" required=""/>
-	  <br></br>
-	  <button class="btn btn-lg btn-primary btn-block" type="submit">Kayıt Ol</button>
-	  <p></p>
-	  <a href="login.php">Giriş Yapın!</a>   
-    </form>
-  </div>
+<div class="form">
+<h1>Kayıt Olun</h1>
+<form name="registration" action="" method="post">
+
+<input type="text" name="username" placeholder="Username" required />
+<br></br>
+<input type="password" name="password" placeholder="Password" required />
+<br></br>
+<input type="text" name="affiliation" placeholder="Company" required />
+<br></br>
+<input type="text" name="title" placeholder="Title" required />
+<br></br>
+<input type="text" name="personName" placeholder="Name" required />
+<br></br>
+<input type="text" name="personSurname" placeholder="Surname" required />
+<br></br>
+<input type="email" name="email1" placeholder="Email" required />
+<br></br>
+<input type="email" name="email2" placeholder="Email" required />
+<br></br>
+<input type="tel" name="phone" placeholder="Phone Number" required />
+<br></br>
+<input type="tel" name="fax" placeholder="Fax Number" required />
+<br></br>
+<input type="text" name="address" placeholder="Address" required />
+<br></br>
+<input type="text" name="country" placeholder="Country" required />
+<br></br>
+<input type="text" name="city" placeholder="City" required />
+<br></br>
+<input type="submit" name="submit" value="Register" />
+</form>
+</div>
+<?php } ?>
+</body>
+</html>
