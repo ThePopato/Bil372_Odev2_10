5802629a61a835ecf09a12ab8cad311abd6837e8
ThePopato <72664761+ThePopato@users.noreply.github.com>
2020-12-02 19:53:38 +0300

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -27,15 +27,21 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
         <br></br>
-        <a href="index.php" class="btn btn-danger">Link to index.php</a>
+        <a href="index.php" class="btn btn-danger">Main Page</a>
         <br></br>
-        <a href="adminDisplayUsers.php" class="btn btn-danger">Link to adminDisplayUsers.php</a>
+        <a href="index_list.php" class="btn btn-danger">List</a>
         <br></br>
-        <a href="index_conference.php" class="btn btn-danger">Link to index_conference.php</a>
+        <a href="index_user.php" class="btn btn-danger">Users</a>
         <br></br>
-        <a href="index_conference_tags.php" class="btn btn-danger">Link to adminDisplayUsers.php</a>
+        <a href="usersinfo_index.php" class="btn btn-danger">User Information</a>
         <br></br>
-        <a href="index_submissions.php" class="btn btn-danger">Link to index_submissions.php</a>
+        <a href="adminDisplayUsers.php" class="btn btn-danger">User Display</a>
+        <br></br>
+        <a href="index_conference.php" class="btn btn-danger">Conferences</a>
+        <br></br>
+        <a href="index_conference_tags.php" class="btn btn-danger">Conference Tags</a>
+        <br></br>
+        <a href="index_submissions.php" class="btn btn-danger">Submissions</a>
     </p>
 </body>
 </html>
